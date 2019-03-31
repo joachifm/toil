d51b7099a504d01ff26520e432204e69ab7c94bc
Joachim Fasting <joachifm@fastmail.fm>
2019-03-31 09:25:17 +0200

    t/test_parse.sh: fix

--- t/test_parse.sh
+++ t/test_parse.sh
@@ -3,7 +3,7 @@ testlog=$(mktemp)
 for x in ./*.txt ; do
     name=$(basename "$x" .txt)
     echo -n "running test: $name ... " >&2
-    tcc -run ../c.c < "$x" &> "$testlog"
+    tcc ../lex.c -run ../c.c < "$x" &> "$testlog"
     ret=$?
     if [[ $ret != 0 ]] ; then
         echo "failed" >&2
