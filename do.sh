991ba6492bc2e4d21de070eee7654f229965de61
Joachim Fasting <joachifm@fastmail.fm>
2019-04-22 21:02:42 +0200

    do: store build outputs under build/

--- do.sh
+++ do.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
-./gen_label_table.sh > labels_gen.h
-c++ -DPARSER_TEST_MAIN -fvisibility=hidden -fno-exceptions -fno-rtti -o parser_test parser.cc
+mkdir -p build/include build/bin
+./gen_label_table.sh > build/include/labels_gen.h
+c++ -DPARSER_TEST_MAIN -Ibuild/include -fvisibility=hidden -fno-exceptions -fno-rtti -o build/bin/parser_test parser.cc
