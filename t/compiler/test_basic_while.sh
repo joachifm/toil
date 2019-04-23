04cfd8174fe5af400cb5a66a3731b0032d7a9a7f
Joachim Fasting <joachifm@fastmail.fm>
2019-04-23 23:24:24 +0200

    basic test for WHILE

---
+++ t/compiler/test_basic_while.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+./tfront <<EOF
+PROGRAM basicwhile
+
+VAR x INT
+
+x := 10
+
+WHILE x
+  x := x - 1
+ENDWHILE
+
+END
+EOF
+
+gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)x' a.out | grep -q '$1 = 0'
