2f315f1f60667dab59e1c180d5a62aef078f7fb4
Joachim Fasting <joachifm@fastmail.fm>
2019-04-23 23:23:16 +0200

    test for subtraction

---
+++ t/compiler/test_basic_arith_sub.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+./tfront <<EOF
+PROGRAM basicarithsub
+
+VAR x INT
+
+x := 2
+x := x - 2
+
+END
+EOF
+
+gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)x' a.out | grep -q '$1 = 0'
