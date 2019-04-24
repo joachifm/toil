a2f812bef49bfdda57bade8b32df3088dd703699
Joachim Fasting <joachifm@fastmail.fm>
2019-04-24 06:00:49 +0200

    test driver: check failure count

--- t/compiler/run_all.sh
+++ t/compiler/run_all.sh
@@ -1,10 +1,19 @@
 #! /usr/bin/env bash
 
+declare -i expected_fail_count=0
+declare -i fail_count=0
+
 for x in t/compiler/test_* ; do
     echo -n "$x: " >&2
     if ./$x ; then
         echo "ok" >&2
     else
         echo "fail" >&2
+        ((++fail_count))
     fi
 done
+
+if (($fail_count > $expected_fail_count)) ; then
+    echo "exceeded expected failure count" >&2
+    exit 1
+fi
