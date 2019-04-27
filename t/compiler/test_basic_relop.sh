03d1da792033e203613b4fbe223e70a82b9478bc
Joachim Fasting <joachifm@fastmail.fm>
2019-04-27 08:29:28 +0200

    more tests for relops

--- t/compiler/test_basic_relop.sh
+++ t/compiler/test_basic_relop.sh
@@ -5,10 +5,16 @@ set -euo pipefail
 PROGRAM basicarith_div
 
 VAR x INT
+VAR y INT
+VAR z INT
 
-x := 4 > 0
+x := 4 > 2
+y := 2 < 4
+z := 4 = 4
 
 END
 EOF
 
 gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)x' a.out | grep -q '$1 = 1$'
+gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)y' a.out | grep -q '$1 = 1$'
+gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)z' a.out | grep -q '$1 = 1$'
