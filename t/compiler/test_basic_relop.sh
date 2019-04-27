de6d6bde4dd0c63c4682cad1c9dfe1a8525da3fb
Joachim Fasting <joachifm@fastmail.fm>
2019-04-27 07:32:45 +0200

    add failing test for relops

---
+++ t/compiler/test_basic_relop.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+./tfront <<EOF
+PROGRAM basicarith_div
+
+VAR x INT
+
+x := 4 > 0
+
+END
+EOF
+
+gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)x' a.out | grep -q '$1 = 1$'
