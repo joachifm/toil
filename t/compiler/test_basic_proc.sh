30c1b6b5caabb4aab18ca762e505c6d295c91374
Joachim Fasting <joachifm@fastmail.fm>
2019-04-28 23:04:55 +0200

    test for procdecl

---
+++ t/compiler/test_basic_proc.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+./tfront <<EOF
+PROGRAM procdecl
+
+VAR x INT
+
+PROC square_x
+  x := 10
+END
+
+END
+EOF
+
+gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)x' a.out | grep -q '$1 = 0$'
