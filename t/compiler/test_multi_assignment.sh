5cd923ad9c2621c15b24c2af7daca3bd9f67d7e4
Joachim Fasting <joachifm@fastmail.fm>
2019-04-28 10:44:39 +0200

    test_multi_assignment: look at all vars

--- t/compiler/test_multi_assignment.sh
+++ t/compiler/test_multi_assignment.sh
@@ -10,10 +10,12 @@ VAR y INT
 VAR z INT
 
 x := 1
-y := 2
-z := x
+y := x
+z := y
 
 END
 EOF
 
+gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)x' a.out | grep -q '$1 = 1$'
+gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)y' a.out | grep -q '$1 = 1$'
 gdb -batch -ex 'break _end_of_program' -ex 'run' -ex 'p (int)z' a.out | grep -q '$1 = 1$'
