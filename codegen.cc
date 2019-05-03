80478e7e4c2b88edaed17312356249beee7efc0c
Joachim Fasting <joachifm@fastmail.fm>
2019-05-03 20:08:17 +0200

    codegen: define a struct for items

--- codegen.cc
+++ codegen.cc
@@ -5,6 +5,25 @@
 
 namespace codegen {
 
+struct Item {
+    enum Kind : unsigned {
+        KIND_CON,
+        KIND_REG,
+        KIND_VAR,
+    } t;
+
+    union {
+        // when KIND_CON
+        struct { int val; } con;
+
+        // when KIND_REG
+        struct { int a; int b; } reg;
+
+        // when KIND_VAR
+        struct { int adr; } var;
+    };
+};
+
 auto next_label() -> char const* {
     static unsigned next;
     if (next > label_count_max - 1) error("exhausted labels");
