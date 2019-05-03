b1b86f9b272f0883731f47e54bba25f8018e71f5
Joachim Fasting <joachifm@fastmail.fm>
2019-05-03 14:28:39 +0200

    build: pass CXXFLAGS from env

--- do.sh
+++ do.sh
@@ -2,4 +2,4 @@
 
 mkdir -p build/include build/bin
 ./gen_label_table.sh > build/include/labels_gen.h
-c++ -Ibuild/include -o build/bin/toilc main.cc
+c++ -Ibuild/include -o build/bin/toilc $CXXFLAGS main.cc
