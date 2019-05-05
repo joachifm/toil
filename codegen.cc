338191f8619beb0184af1a0f1a531c492d7389ea
Joachim Fasting <joachifm@fastmail.fm>
2019-05-05 23:57:31 +0200

    cgen: make labels table local to next_label

--- codegen.cc
+++ codegen.cc
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "aux.hh"
-#include "labels_gen.h"
 
 namespace codegen {
 
 auto next_label() -> char const* {
+#include "labels_gen.h"
     static unsigned next;
     if (next > label_count_max - 1) error("exhausted labels");
     unsigned ind = next++;
